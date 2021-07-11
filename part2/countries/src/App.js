44d157f6fb7c5740c0bc5e503273776f35a5be00
Lawrence (Drew) Whisenant <7390131+drewwhis@users.noreply.github.com>
2021-07-11 16:00:48 -0500

    finish 2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -12,6 +12,11 @@ const Search = ({ search, handleSearchChange }) => {
 };
 
 const CountryInfo = ({ country }) => {
+  if (country === null) {
+    return (
+      <></>
+    );
+  }
   const altText = `Flag of ${country.name}`;
   return (
     <>
@@ -27,7 +32,7 @@ const CountryInfo = ({ country }) => {
   );
 };
 
-const Results = ({ search, countries }) => {
+const Results = ({ search, countries, selectCountry, country }) => {
   const matches = countries.filter(c => c.name.toLowerCase().includes(search.toLowerCase()));
   if (search.length === 0 || matches.length === 0) {
     return (
@@ -41,10 +46,19 @@ const Results = ({ search, countries }) => {
     );
   }
 
+  const generateClickHandler = country => {
+    return () => {
+      selectCountry(country);
+    };
+  };
+
   if (matches.length > 1) {
     return (
       <>
-        {matches.map(c => <p key={c.name}>{c.name}</p>)}
+        {matches.map(c =>
+          <div key={c.name}>{c.name} <button onClick={generateClickHandler(c)}>show</button></div>
+        )}
+        <CountryInfo country={country} />
       </>
     );
   }
@@ -59,6 +73,7 @@ const Results = ({ search, countries }) => {
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState('');
+  const [country, setCountry] = useState(null);
 
   useEffect(() => {
     axios
@@ -68,16 +83,18 @@ const App = () => {
       });
   }, []);
 
-
-
   const handleSearchChange = (event) => {
     setSearch(event.target.value);
+  };
+
+  const handleCountrySelect = selectedCountry => {
+    setCountry(selectedCountry);
   };
 
   return (
     <>
       <Search search={search} handleSearchChange={handleSearchChange} />
-      <Results search={search} countries={countries} />
+      <Results search={search} countries={countries} selectCountry={handleCountrySelect} country={country} />
     </>
   );
 };
